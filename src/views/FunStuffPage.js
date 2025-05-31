e9584bac73731657b2d633fe6894af6b3a246872
Beatrice Gan <Beatrice@MacBook-Air-2.local>
2025-05-31 21:30:56 +0800

    fun stuff page padding and align

--- src/views/FunStuffPage.js
+++ src/views/FunStuffPage.js
@@ -1,11 +1,14 @@
 import { Typography } from "@mui/material";
+import Grid from '@mui/material/Grid2';
 
 function FunStuffPage() {
     return (
         <div>
-            <Typography variant="body1" className="subheading" sx={{mt:"10vh"}}>
-                Stay tuned to more cool stuff I plan to add here!
-            </Typography>
+            <Grid container spacing={2} sx={{ p:2 }}>
+                <Typography variant="body1" className="subheading" sx={{mt:"10vh", textAlign: "center"}}>
+                    Stay tuned to more cool stuff I plan to add here!
+                </Typography>
+            </Grid>
         </div>
     )
 }
